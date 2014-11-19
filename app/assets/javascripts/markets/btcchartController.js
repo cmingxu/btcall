246ac498dd7147121cc55910406bc774d499075f
Kevin.Xu <cming.xu@gmail.com>
2014-11-19 13:25:04 +0800

    [ADDED] more verbose socket io event added

--- app/assets/javascripts/markets/btcchartController.js
+++ app/assets/javascripts/markets/btcchartController.js
@@ -1,38 +1,36 @@
 market.controller("btcchartController", ["$scope", "btcSocket", function ($scope, btcSocket) {
-  //connect connect_error connect_timeout reconnect reconnect_attempt reconnect_error
-  //reconnect_failed
+  socket_events = ["connect", "connect_error", "connect_timeout",
+    "reconnect", "reconnect_attempt", "reconnect_error", "econnect_failed"];
 
-  $scope.data = [];
-  $scope.chart_dom = null;
+    $scope.data = [];
+    $scope.chart_dom = null;
 
-  btcSocket.on('connect', function () {
-    console.log("just connected");
-  });
+    angular.forEach(socket_events, function (e) {
+      btcSocket.on(e, function () {
+        console.log(e);
+      })
+    });
 
-  btcSocket.on('reconnect_attempt', function () {
-    console.log("reconnect_attempt");
-  });
+    btcSocket.on('message:batch', function (msg) {
+      $scope.data = msg;
+    });
 
-  btcSocket.on('message:batch', function (msg) {
-    $scope.data = msg;
-  });
+    btcSocket.on('message:single', function (msg) {
+      $scope.data.unshift(msg);
+      console.log($scope.data.length);
+    });
 
-  btcSocket.on('message:single', function (msg) {
-    $scope.data.unshift(msg);
-    console.log($scope.data.length);
-  });
+    $scope.draw_btc_chart = function () {
+      $scope.chart_dom = $("#btcchart");
+    }
 
-  $scope.draw_btc_chart = function () {
-    $scope.chart_dom = $("#btcchart");
-  }
-
-  $scope.redraw_btc_chart = function () {
-    var svg = dimple.newSvg(chart_dom.get(0), chart_dom.width(), chart_dom.height());
-    chart = new dimple.chart(svg, $scope.data);
-    chart.addCategoryAxis("x", "timestamp");
-    chart.addMeasureAxis("y", "value");
-    chart.addSeries(null, dimple.plot.area);
-    chart.draw();
-  }
+    $scope.redraw_btc_chart = function () {
+      var svg = dimple.newSvg(chart_dom.get(0), chart_dom.width(), chart_dom.height());
+      chart = new dimple.chart(svg, $scope.data);
+      chart.addCategoryAxis("x", "timestamp");
+      chart.addMeasureAxis("y", "value");
+      chart.addSeries(null, dimple.plot.area);
+      chart.draw();
+    }
 
 }]);
